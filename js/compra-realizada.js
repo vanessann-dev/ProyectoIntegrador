628c98025b40cbaaf2d53e130476da0b69f51b33
Lina Naranjo <lina.naja1807@gmail.com>
2024-02-14 22:39:06 -0500

    Ultimos detalles Vanessa y Lina

--- js/compra-realizada.js
+++ js/compra-realizada.js
@@ -1,134 +1,144 @@
-const orderId = sessionStorage.getItem("idOrden");
-let ordenActual = null;
+const compra = JSON.parse(sessionStorage.getItem("compra"));
+
+// Verificar si la compra existe
+
+const imprimirFactura = () => {
+  const contenedorAImprimir = document.querySelector(".contenedorDatos");
+  contenedorAImprimir.innerHTML = "";
+const numeroOrden = generarNumeroOrden();
+  const nuevoHtmlCompras = `
+    <div class="linea">
+      <div class="datos-compra">
+        <div class="especificaciones-compra">
+          <img src="../Assets/icono calendario.png" alt="icono-calendario" />
+          <p>Date</p>
+        </div>
+        <div class="contenedor-parrafo">
+          <p>${compra.fechaCompra}</p>
+        </div>
+      </div>
+      <div class="datos-compra">
+        <div class="especificaciones-compra">
+          <img src="../Assets/icono perfil.png" alt="icono-perfil" />
+          <p>Customer</p>
+        </div>
+        <div>
+          <p class="contenedor-parrafo">
+            <strong>${compra.nombre}</strong>
+          </p>
+        </div>
+      </div>
+      <div class="datos-compra">
+        <div class="especificaciones-compra">
+          <img src="../Assets/icono pago.png" alt="icono-pago" />
+          <p>Payment Method</p>
+        </div>
+        <div class="contenedor-parrafo">
+          <p>${compra.tipoBanco}</p>
+        </div>
+      </div>
+    </div>
+    <div class="linea">
+      <div class="datos-compra">
+        <div class="especificaciones-compra">
+          <img src="../Assets/icono factura.png" alt="icono-factura" />
+          <p>Order Number</p>
+        </div>
+        <div class="contenedor-parrafo">
+          <p>${numeroOrden}</p> <!-- Asegúrate de tener la propiedad id en el objeto compra -->
+        </div>
+      </div>
+      <div class="datos-compra">
+        <div class="especificaciones-compra">
+          <img src="../Assets/icono peso.png" alt="icono-peso" />
+          <p>Total</p>
+        </div>
+        <div class="contenedor-parrafo">
+          <p id="totalCompraFactura">$${compra.total}</p> <!-- Asegúrate de tener la propiedad total en el objeto compra -->
+        </div>
+      </div>
+    </div>
+  `;
+
+  contenedorAImprimir.innerHTML = nuevoHtmlCompras;
+};
+
+function generarNumeroOrden() {
+    const numeroOrden = Math.floor(Math.random() * 900000) + 100000;
+    return numeroOrden;
+  }
+
+// Llamar a la función para imprimir la factura
+imprimirFactura();
+
+//pintar datos 
+const contenedorProductos = document.getElementById("contenedorImagenesCompra");
+
+const pintarCarritoCompras = () => {
+  contenedorProductos.innerHTML = "";
+  
+  // Recuperar productos del sessionStorage
+  const productosEnCarrito = JSON.parse(sessionStorage.getItem("productosEnCarrito"));
+  
+  if (!productosEnCarrito || productosEnCarrito.length === 0) {
+    contenedorProductos.innerHTML = "<p>No hay productos en el carrito</p>";
+    return;
+  }
+
+  productosEnCarrito.forEach((item) => {
+    contenedorProductos.innerHTML += `
+      <div class="detalle-producto">
+        <div class="datos-compra-detalle-producto">
+          <img class="imagenCompra"
+            src="${item.producto.imagenes}"
+            alt="${item.producto.nombre}"
+          />
+          <div class="especificaciones-compra-img">
+            <h3>${item.producto.nombre}</h3>
+            <p>${item.producto.codigo}</p>
+            <button>x${item.cantidad}</button>
+          </div>
+        </div>
+        <div class="contenedor-parrafo-img">
+          <p><strong>$${item.producto.precioUnitario}</strong></p>
+        </div>
+      </div>
+    `;
+  });
+};
+
+// Llamar a la función para que pinte el carrito al cargar la página
+pintarCarritoCompras();
+ 
 
 
-const consultarOrden = async() =>{
-    const orden = await fetch("http://localhost:3000/ordenesCompra" +"/" + orderId);
-    const respuesta = await orden.json();
-    ordenActual = respuesta;
-    console.log(ordenActual);
+//calcular el total de la compra
+function sumarPreciosProductos() {
+  let total = 0;
+
+  const productosEnCarrito =
+    JSON.parse(sessionStorage.getItem("productosEnCarrito")) || [];
+
+  productosEnCarrito.forEach((producto) => {
+    total += producto.producto.precioUnitario * producto.cantidad;
+  });
+  // console.log(productosEnCarrito)
+  return total;
 }
 
-consultarOrden();
+function mostrarTotalCompra(total) {
+  const elementoTotal = document.getElementById("totalCompraFactura");
+  if (elementoTotal) {
+    elementoTotal.textContent = "$" + total.toFixed(2); // Formatear el total como moneda
+  }
+}
 
+const totalComprasFactura = sumarPreciosProductos();
+mostrarTotalCompra(totalComprasFactura);
 
+const botonLimpiar = document.getElementById("botonLimpiar");
 
-
-// const orderId = sessionStorage.getItem("idOrden");
-// let ordenActual = null;
-
-// const consultarOrden = async () => {
-//   try {
-//     const response = await fetch("http://localhost:3000/ordenesCompra" + "/" + orderId);
-//     const orden = await response.json();
-//     ordenActual = orden;
-//     console.log(ordenActual);
-
-//     // Mostrar los detalles de la orden en la interfaz de usuario
-//     mostrarDetallesOrden(ordenActual);
-//   } catch (error) {
-//     console.error('Error al consultar la orden:', error);
-//   }
-// };
-
-// const mostrarDetallesOrden = (orden) => {
-//   // Aquí puedes escribir la lógica para mostrar los detalles de la orden en tu interfaz de usuario
-//   // Por ejemplo, actualizar los elementos HTML con los datos de la orden
-//   const dateElement = document.getElementById("date");
-//   const customerElement = document.getElementById("contenedor-parrafo");
-//   const paymentMethodElement = document.getElementById("paymentMethod");
-//   const orderNumberElement = document.getElementById("orderNumber");
-//   const totalElement = document.getElementById("total");
-
-//   if (dateElement) {
-//     dateElement.textContent = orden.fechaCompra; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de orden
-//   }
-//   if (customerElement) {
-//     customerElement.textContent = orden.nombre; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de orden
-//   }
-//   if (paymentMethodElement) {
-//     paymentMethodElement.textContent = orden.tipoBanco; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de orden
-//   }
-//   if (orderNumberElement) {
-//     orderNumberElement.textContent = orderId;
-//   }
-//   if (totalElement) {
-//     // Aquí puedes calcular el total de la orden en función de los productos en el carrito
-//     // y luego actualizar el elemento HTML correspondiente
-//   }
-
-//   // Mostrar los detalles de los productos asociados a la orden
-//   const productosEnCarrito = orden.productosEnCarrito;
-//   const productosContainer = document.getElementById("productosContainer");
-
-//   if (productosEnCarrito && productosContainer) {
-//     productosEnCarrito.forEach(producto => {
-//       const productoElement = document.createElement("div");
-//       productoElement.classList.add("detalle-producto");
-
-//       const productoDetalleElement = document.createElement("div");
-//       productoDetalleElement.classList.add("datos-compra-detalle-producto");
-
-//       const imagenElement = document.createElement("img");
-//       imagenElement.src = `../Assets/${producto.producto.nombre}.png`; // Asegúrate de que la ruta de la imagen sea correcta
-//       imagenElement.alt = producto.producto.nombre; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de producto
-
-//       const especificacionesElement = document.createElement("div");
-//       especificacionesElement.classList.add("especificaciones-compra-img");
-
-//       const nombreElement = document.createElement("h3");
-//       nombreElement.textContent = producto.producto.nombre; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de producto
-
-//       const codigoElement = document.createElement("p");
-//       codigoElement.textContent = `Code: ${producto.producto.codigo}`; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de producto
-
-//       const precioElement = document.createElement("p");
-//       precioElement.textContent = `$${producto.producto.precioUnitario}`; // Asegúrate de que el nombre de la propiedad sea correcto en tu objeto de producto
-
-//       const cantidadElement = document.createElement("button");
-//       cantidadElement.textContent = `x${producto.cantidad}`;
-
-//       productoDetalleElement.appendChild(imagenElement);
-//       especificacionesElement.appendChild(nombreElement);
-//       especificacionesElement.appendChild(codigoElement);
-//       especificacionesElement.appendChild(cantidadElement);
-//       productoDetalleElement.appendChild(especificacionesElement);
-//       productoElement.appendChild(productoDetalleElement);
-
-//       const precioContainer = document.createElement("div");
-//       precioContainer.classList.add("contenedor-parrafo-img");
-
-//       const precioStrong = document.createElement("p");
-//       precioStrong.innerHTML = `<strong>${precioElement.textContent}</strong>`;
-
-//       precioContainer.appendChild(precioStrong);
-//       productoElement.appendChild(precioContainer);
-
-//       productosContainer.appendChild(productoElement);
-//     });
-//   }
-// };
-
-// consultarOrden();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const consultarOrden = async() =>{
-//     const orden = await fetch("http://localhost:3000/ordenesCompra" +"/" + orderId);
-//     const respuesta = await orden.json();
-//     ordenActual = respuesta;
-//     console.log(ordenActual);
-// }
-
-// consultarOrden();
-
+botonLimpiar.addEventListener("click", function() {
+  sessionStorage.clear();
+  window.location.href = "../pages/shop.html"
+});
